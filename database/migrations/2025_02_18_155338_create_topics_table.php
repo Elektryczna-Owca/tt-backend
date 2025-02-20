2aa30c2b55c0cfa2ec4b47e2f47184659e992970
Radosław Malinowski <malinowski.rad@gmail.com>
2025-02-20 17:09:43 +0100

    docker use mysql instead pgsql

--- database/migrations/2025_02_18_155338_create_topics_table.php
+++ database/migrations/2025_02_18_155338_create_topics_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('topics', function (Blueprint $table) {
             $table->id();
-            $table->string('topic_name');
+            $table->string('name');
             $table->string('description');
             $table->timestamps();
         });
